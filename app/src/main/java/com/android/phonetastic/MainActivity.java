d49c545d9dd8555d8ae88d8bd704114a44f65eb7
CarissimiMichele <m.carissimi4@campus.unimib.it>
2021-11-22 16:41:24 +0100

    Initial commit

---
+++ app/src/main/java/com/android/phonetastic/MainActivity.java
@@ -0,0 +1,24 @@
+package com.android.phonetastic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.android.phonetastic.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+}
